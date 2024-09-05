25ed56754872614d7f49f830b7483db26d1a3dcd
Eric Sexton <foopis23@gmail.com>
2024-09-05 17:17:52 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# MC Cool Points 
+Cool Points is a data pack where players can earn "cool points" by finding an hidden objective.
+
+This is based off an old minecraft server that I use to play with my friends where I would set a random objective criteria every so often and make it so players have to figure out what it is to get more score. Mostly just for bragging rights. This data pack is a recreation of that idea.
+
+This is still in early development so it isn't really plug and play quite yet and the "game play" isn't fleshed out yet
---
+++ data-pack.config.ts
@@ -0,0 +1,31 @@
+import { packConfig, functionConfig, command } from "ts-datapacks";
+import { BLOCK_IDS } from "./blocks";
+
+export default packConfig({
+  namespace: "cool_points",
+  functions: [
+    functionConfig({
+      name: "setup",
+      command: command`
+        summon armor_stand 0 318 0 {Invisible:1b,Invulnerable:1b,NoAI:1b,NoGravity:1b,Silent:1b,Tags:["cool_points_data"]}
+        scoreboard objectives add current_objective dummy
+      `,
+    }),
+    functionConfig({
+      name: "update_objective",
+      command: command`
+        execute store result score @n[tag=cool_points_data] current_objective run random value 0 ${
+          BLOCK_IDS.length
+        }
+        scoreboard objectives remove cool_points
+      
+        ${BLOCK_IDS.map(
+          (blockId, index) =>
+            command`execute if score @n[tag=cool_points_data] current_objective matches ${
+              index + 1
+            } run scoreboard objectives add cool_points mined:${blockId}`
+        )}
+      `,
+    }),
+  ],
+});
