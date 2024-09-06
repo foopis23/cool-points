9b14105b2d9bce2840f37fc36b6d0f412ea83eab
Eric Sexton <foopis23@gmail.com>
2024-09-06 14:00:38 -0400

    announce function and set display

--- data-pack.config.ts
+++ data-pack.config.ts
@@ -15,12 +15,29 @@ export default packConfig({
     functionConfig({
       name: "update_objective",
       command: command`
-        execute store result score @n[tag=cool_points_data] current_objective run random value 0..${BLOCK_IDS.length}
+        execute store result score @n[tag=cool_points_data] current_objective run random value 0..${
+          BLOCK_IDS.length
+        }
         scoreboard objectives remove cool_points
 
         ${BLOCK_IDS.map(
           (blockId, index) =>
-            command`execute if score @n[tag=cool_points_data] current_objective matches ${index + 1} run scoreboard objectives add cool_points mined:${blockId}`
+            command`execute if score @n[tag=cool_points_data] current_objective matches ${
+              index + 1
+            } run scoreboard objectives add cool_points mined:${blockId}`
+        )}
+
+        scoreboard objectives setdisplay sidebar cool_points
+      `,
+    }),
+    functionConfig({
+      name: "announce_current_objective",
+      command: command`
+        ${BLOCK_IDS.map(
+          (blockId, index) =>
+            command`execute if score @n[tag=cool_points_data] current_objective matches ${
+              index + 1
+            } run tellraw @a {"text":"Mine ","color":"gray","extra":[{"text":"${blockId}","color":"dark_gray"}]}`
         )}
       `,
     }),
